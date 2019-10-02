2398027306f161bcad6cb05ac64c0f06d580d706
magicianA <a1awsdadsdasd@126.com>
2019-10-02 17:13:39 +0800

    update10.2

---
+++ Assets/Scripts/Mainscene/Snakehead.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snakehead : MonoBehaviour
+{
+    private float speed = 1f;
+    void Start()
+    {
+         
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.transform.position += Vector3.right;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Startscene/exit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class exit : MonoBehaviour
+{
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
