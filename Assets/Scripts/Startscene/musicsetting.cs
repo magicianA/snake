d357f0779882e146a4c9a4128d38199b15b3d2b1
magicianA <a1awsdadsdasd@126.com>
2019-10-07 23:09:33 +0800

    complete

---
+++ Assets/Scripts/Startscene/musicsetting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class musicsetting : MonoBehaviour
+{
+
+    public Slider slider;
+    public AudioSource s;
+    void Awake()
+    {
+        s = GetComponent<AudioSource>();
+    }    
+    void Update()
+    {
+        s.volume = slider.value;
+    }
+}
+
---
+++ Assets/Scripts/Startscene/setting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setting : MonoBehaviour
+{
+    public GameObject menu;
+    public void control()
+    {
+        menu.SetActive(true);
+    }
+    public void closecontrol()
+    {
+        menu.SetActive(false);
+    }
+}
